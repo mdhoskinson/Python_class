95b5e95994f6ad055c735bcb92610744c62f852d
mdhoskinson <105821406+mdhoskinson@users.noreply.github.com>
2022-05-18 13:29:42 -0700

    Add files via upload

---
+++ Assigment05_Starter.py
@@ -0,0 +1,73 @@
+# ------------------------------------------------------------------------ #
+# Title: Assignment 05
+# Description: Working with Dictionaries and Files
+#              When the program starts, load each "row" of data
+#              in "ToDoToDoList.txt" into a python Dictionary.
+#              Add the each dictionary "row" to a python list "table"
+# ChangeLog (Mellony Hoskinson,5/17/2022, Processing and Input):
+# RRoot,1.1.2030,Created started script
+# Mellony Hoskinosn, May 17, 2022. Added code to complete assignment 5
+# ------------------------------------------------------------------------ #
+
+# -- Data -- #
+# declare variables and constants
+# objFile = "ToDoList.txt"   # An object that represents a file
+strData = ""  # A row of text data from the file
+dicRow = {}    # A row of data separated into elements of a dictionary {Task,Priority}
+lstTable = []  # A list that acts as a 'table' of rows
+strMenu = ""   # A menu of user options
+strChoice = "" # A Capture the user option selection
+strFile = "ToDoFile.txt"
+
+# -- Processing -- #
+# Step 1 - When the program starts, load the any data you have
+# in a text file called ToDoList.txt into a python list of dictionaries rows (like Lab 5-2)
+dicRow1 = {"Task": "1", "Priority": "High"}
+dicRow2 = {"Task": "2", "Priority": "Low"}
+lstTable = [dicRow1, dicRow2]
+objFile = open(strFile, "r")
+for row in objFile:
+    dicRow = {"Task": "1", "Priority": "1"}
+    print(dicRow)
+    print(dicRow["Task"] + ',' + dicRow["Priority"])
+    lstTable.append(dicRow)
+  objFile.close()
+
+
+# -- Input/Output -- #
+# Step 2 - Display a menu of choices to the user
+while (True):
+    print("""
+    Menu of Options
+    1) Show current data
+    2) Add a new item.
+    3) Remove an existing item.
+    4) Save Data to File
+    5) Exit Program
+    """)
+    strChoice = str(input("Which option would you like to perform? [1 to 5] - "))
+    print()  # adding a new line for looks
+    # Step 3 - Show the current items in the table
+    if (strChoice.strip() == '1'):
+        print(lstTable)
+        continue
+    # Step 4 - Add a new item to the list/Table
+    elif (strChoice.strip() == '2'):
+        strTask = input("Enter a Task: ")
+        strPriority = input("Enter a Priority of High, Medium, or Low: ")
+        dicRow = {"Task": "1", "Priority": "1"}
+        lstTable.append(dicRow)
+        for lstRow in lstTable:
+            print(lstRow)
+        continue
+    # Step 5 - Remove a new item from the list/Table
+    elif (strChoice.strip() == '3'):
+       del dicRow[1]
+        continue
+    # Step 6 - Save tasks to the ToDoToDoList.txt file
+    elif (strChoice.strip() == '4'):
+        # TODO: Add Code Here
+        continue
+    # Step 7 - Exit program
+    elif (strChoice.strip() == '5'):
+        break  # and Exit the program
